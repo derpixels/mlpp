2f9f89d5372fcc6a4833b6f387fc746a9cfee0aa
derpixels <46532452+derpixels@users.noreply.github.com>
2019-01-09 23:51:46 +0300

    Update Vascopixel Minimap.user.js

--- Vascopixel Minimap.user.js
+++ Vascopixel Minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/derpixels/ponehs/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/derpixels/mlpp/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
